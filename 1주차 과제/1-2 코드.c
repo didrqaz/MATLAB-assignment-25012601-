1de73b322be3a4c75c0e39b79687ba7f590c85e6
Gain Yang <didrqaz@gmail.com>
2026-03-09 23:55:38 +0900

    Update 1-2 코드.c

--- 1주차 과제/1-2 코드.c
+++ 1주차 과제/1-2 코드.c
@@ -1,18 +1,17 @@
 #pragma warning(disable:4996)
 #include <stdio.h>
 #include <stdlib.h>
-#include < string.h >
 
 int main() {
 
-	int num, *div;
+	int num, *div;//num: 약수 개수, div: 약수 집합
 	int i, j, tmp;
 	scanf("%d", &num);
 	div = (int*)malloc(num * sizeof(int));
-	for (i = 0; i < num; i++) {
+	for (i = 0; i < num; i++) {//약수 입력받기
 		scanf("%d", &div[i]);
 	}
-	for (i = 0; i < num; i++) {
+	for (i = 0; i < num; i++) {//약수 오름차순 정렬
 		for (j = 0; j < i; j++) {
 			if (div[j] > div[i]) {
 				tmp = div[i];
@@ -21,10 +20,11 @@ int main() {
 			}
 		}
 	}
-	printf("%d", div[0] * div[num - 1]);
+	printf("%d", div[0] * div[num - 1]);//N(= 약수 최소값 * 최대값) 출력
 
 	free(div);
 
 
 	return 0;
+
 }
