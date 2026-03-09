9d746b17f328204031225e53dfaf07a91f6defe8
Gain Yang <didrqaz@gmail.com>
2026-03-09 23:57:44 +0900

    Update 1-3 코드.c

--- 1주차 과제/1-3 코드.c
+++ 1주차 과제/1-3 코드.c
@@ -9,18 +9,19 @@ struct xy {
 
 int main() {
 
-	char str[100];
-	int check = 1;
+	char str[100];//str: 입력받는 단어
+	int check = 1;//check:  팰린드롬인지 여부. 1이면 팰린드롬, 0이면 팰린드롬 아님
 	int i;
 	scanf("%s", str);
-	for (i = 0; i < strlen(str) / 2; i++) {
+	for (i = 0; i < strlen(str) / 2; i++) {//단어의 1/2를 순회하여 양끝의 i번째 단어가 서로 같은지 확인
 		if (str[i] != str[strlen(str) - i - 1]) {
 			check = 0;
 			break;
 		}
 	}
-	printf("%d", check);
+	printf("%d", check);//팰린드롬 여부 출력
 
 
 	return 0;
+
 }
